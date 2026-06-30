ae909b7bc1c2097efbd573b5f53bcf2d0ffc6894
pixelForge-png <jordanbrophy04@gmail.com>
2026-06-29 20:44:31 -0700

    Update test_game.py

--- games/test_game.py
+++ games/test_game.py
@@ -1,1 +1,12 @@
+import time
 
+def run(display=None, buttons=None):
+    print("Test Game started!")
+
+    if display:
+        display.fill(0)
+        display.text("PIXELFORGE", 20, 10, 1)
+        display.text("Test Game", 24, 30, 1)
+        display.show()
+
+    time.sleep(2)
