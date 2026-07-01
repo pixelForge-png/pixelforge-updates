337af2baede995ca77b14f09ea4c22fbd91213fd
pixelForge-png <jordanbrophy04@gmail.com>
2026-07-01 10:21:04 -0700

    Create settings_manager.py

---
+++ systems/settings_manager.py
@@ -0,0 +1,52 @@
+import ujson
+import os
+
+SETTINGS_FILE = "/data/settings.json"
+VERSIONS_FILE = "/data/versions.json"
+
+DEFAULT_SETTINGS = {
+    "username": "PLAYER",
+    "wifi_ssid": "",
+    "wifi_password": "",
+    "auto_update": True,
+    "dev_mode" : False
+}
+
+def ensure_data_folder():
+    try:
+        os.mkdir("/data")
+    except OSError:
+        pass
+
+def load_settings():
+    ensure_data_folder()
+
+    try:
+        with open(SETTINGS_FILE, "r") as f:
+            return ujson.loads(f.read())
+    except:
+        save_settings(DEFAULT_SETTINGS)
+        return DEFAULT_SETTINGS.copy()
+
+def save_settings(settings):
+    ensure_data_folder()
+
+    with open(SETTINGS_FILE, "w") as f:
+        f.write(ujson.dumps(settings))
+
+def load_versions():
+    ensure_data_folder()
+
+    try:
+        with open(VERSIONS_FILE, "r") as f:
+            return ujson.loads(f.read())
+    except:
+        save_versions({})
+        return {}
+
+def save_versions(versions):
+    ensure_data_folder()
+
+    with open(VERSIONS_FILE, "w") as f:
+        f.write(ujson.dumps(versions))
+
