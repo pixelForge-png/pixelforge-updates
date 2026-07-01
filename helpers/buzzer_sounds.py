ffa2eef231ae1b716f57cf919a7acd788221af6e
pixelForge-png <jordanbrophy04@gmail.com>
2026-07-01 10:27:21 -0700

    Create buzzer_sounds.py

---
+++ helpers/buzzer_sounds.py
@@ -0,0 +1,12 @@
+from machine import Pin, PWM
+import time
+
+buzzer = PWM(Pin(19))
+buzzer.freq(440)
+buzzer.duty_u16(0)
+
+def sound(frequency, volume, t):
+    buzzer.freq(frequency)
+    buzzer.duty_u16(volume)
+    time.sleep(t)
+    buzzer.duty_u16(0)
