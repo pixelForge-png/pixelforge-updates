36e0601b304f0dd8f030fbe2c212bbcfa11f0ac8
pixelForge-png <jordanbrophy04@gmail.com>
2026-07-11 19:36:38 -0700

    Update pixel_adventure.py

--- games/pixel_adventure.py
+++ games/pixel_adventure.py
@@ -1,6 +1,14 @@
 import time
 import random
 from helpers.buzzer_sounds import sound
+from helpers.pixel_adventure_helper import (
+    BLACK, WHITE, GRAY, DARK_GRAY, RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA,
+    ORANGE, PURPLE, HEALTH_GREEN, DANGER_RED, SHIELD_BLUE, SPACE_BLACK,
+    ENEMY_BULLET, PLAYER_BULLET, LASER_COLOR, FREEZE_COLOR, BOMB_COLOR,
+    GAME_PALETTE,
+    center_text_x, draw_tiny_text, draw_color_sprite, draw_sprite, show_display,
+    joystick_direction, joystick_pressed, wait_for_joystick_release
+)
 
 
 map: [
